5c4602b765837805921d37a1ec46e9a92b7abe0c
xikxekxok <tommykulapin@yandex.ru>
2016-11-20 13:18:36 +0300

    add webpack

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
+const webpack = require('gulp-webpack');
 
 gulp.task('style', function () {
   gulp.src('sass/style.scss')
@@ -38,13 +39,19 @@ gulp.task('style', function () {
 });
 
 gulp.task('scripts', function () {
-  gulp.src('js/**/*.js')
+  gulp.src('js/main.js')
     .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['es2015']
+    .pipe(webpack({
+      devtool:'source-map',
+      module: {
+        loaders: [
+          { test: /\.js$/, loader: 'babel-loader' },
+        ],
+      },
+      output:{
+        filename:'main.js'
+      }
     }))
-    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build/js/'));
 });
 
