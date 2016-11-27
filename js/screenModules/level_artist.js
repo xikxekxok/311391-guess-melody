142c9f91284703d29b4f649ff2a4c80f921e3755
xikxekxok <tommykulapin@yandex.ru>
2016-11-27 19:56:22 +0300

    complete task 6

--- js/screenModules/level_artist.js
+++ js/screenModules/level_artist.js
@@ -3,57 +3,62 @@ import setScreen from '../currentScreenProvider';
 import openGenreScreen from './level_genre';
 import {registerClickHandler} from '../domHelper';
 
-const getLevelArtistScreen = () => getElementFromTemplate(
-    `<section class="main main--level main--level-artist">
-    <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
+const getTimer = (timerModel) =>
+  `<svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
       <circle
         cx="390" cy="390" r="370"
         class="timer-line"
         style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
 
       <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-        <span class="timer-value-mins">02</span><!--
+        <span class="timer-value-mins">${timerModel.mins}</span><!--
         --><span class="timer-value-dots">:</span><!--
-        --><span class="timer-value-secs">00</span>
+        --><span class="timer-value-secs">${timerModel.secs}</span>
       </div>
-    </svg>
+    </svg>`;
 
+const getAnswer = (answerModel) =>
+  `<div class="main-answer-wrapper">
+          <input class="main-answer-r" type="radio" id="answer-${answerModel.id}" name="answer" value="val-1" />
+          <label class="main-answer" for="answer-${answerModel.id}">
+            <img class="main-answer-preview" src="">
+            ${answerModel.text}
+          </label>
+        </div>`;
+
+const getLevelArtistScreen = (model) => getElementFromTemplate(
+    `<section class="main main--level main--level-artist">
+    ${getTimer(model.timer)}
     <div class="main-wrap">
       <div class="main-timer"></div>
 
-      <h2 class="title main-title">Кто исполняет эту песню?</h2>
+      <h2 class="title main-title">${model.question}</h2>
+
       <div class="player-wrapper"></div>
       <form class="main-list">
-        <div class="main-answer-wrapper">
-          <input class="main-answer-r" type="radio" id="answer-1" name="answer" value="val-1" />
-          <label class="main-answer" for="answer-1">
-            <img class="main-answer-preview" src="">
-            Пелагея
-          </label>
-        </div>
-
-        <div class="main-answer-wrapper">
-          <input class="main-answer-r" type="radio" id="answer-2" name="answer" value="val-1" />
-          <label class="main-answer" for="answer-2">
-            <img class="main-answer-preview" src="">
-            Краснознаменная дивизия имени моей бабушки
-          </label>
-        </div>
-
-        <div class="main-answer-wrapper">
-          <input class="main-answer-r" type="radio" id="answer-2" name="answer" value="val-1" />
-          <label class="main-answer" for="answer-2">
-            <img class="main-answer-preview" src="">
-            Lorde
-          </label>
-        </div>
+        ${model.answers.map((x) => getAnswer(x)).join('')}
       </form>
     </div>
   </section>`
 );
 
+const defaultModel = {
+  timer: {
+    mins: 2,
+    secs: 0
+  },
+  question: 'Кто исполняет эту песню?',
+  answers: [
+    {id: 1, text: 'Пелагея'},
+    {id: 2, text: 'Краснознаменная дивизия имени моей бабушки'},
+    {id: 3, text: 'Lorde'}
+  ]
+};
+
 const open = () => {
-  setScreen(getLevelArtistScreen());
+  let screen = getLevelArtistScreen(defaultModel);
+  setScreen(screen);
+
   registerClickHandler('.main-answer-r', openGenreScreen);
 };
 
--- js/screenModules/level_genre.js
+++ js/screenModules/level_genre.js
@@ -3,41 +3,38 @@ import setScreen from '../currentScreenProvider';
 import openResultScreen from './result';
 import {registerClickHandler, registerSubmitHandler} from '../domHelper';
 
-const getLevelGenreScreen = () => getElementFromTemplate(
+
+const getAnswer = (answerModel) =>
+    `<div class="genre-answer">
+        <div class="player-wrapper"></div>
+        <input type="checkbox" name="answer" value="answer-${answerModel.id}" id="a-${answerModel.id}">
+        <label class="genre-answer-check" for="a-${answerModel.id}"></label>
+      </div>`;
+
+const getLevelGenreScreen = (model) => getElementFromTemplate(
     `<section class="main main--level main--level-genre">
-    <h2 class="title">Выберите инди-рок треки</h2>
+    <h2 class="title">${model.question}</h2>
     <form class="genre">
-      <div class="genre-answer">
-        <div class="player-wrapper"></div>
-        <input type="checkbox" name="answer" value="answer-1" id="a-1">
-        <label class="genre-answer-check" for="a-1"></label>
-      </div>
-
-      <div class="genre-answer">
-        <div class="player-wrapper"></div>
-        <input type="checkbox" name="answer" value="answer-1" id="a-2">
-        <label class="genre-answer-check" for="a-2"></label>
-      </div>
-
-      <div class="genre-answer">
-        <div class="player-wrapper"></div>
-        <input type="checkbox" name="answer" value="answer-1" id="a-3">
-        <label class="genre-answer-check" for="a-3"></label>
-      </div>
-
-      <div class="genre-answer">
-        <div class="player-wrapper"></div>
-        <input type="checkbox" name="answer" value="answer-1" id="a-4">
-        <label class="genre-answer-check" for="a-4"></label>
-      </div>
-
+      ${model.answers.map((x) => getAnswer(x)).join('')}
       <button class="genre-answer-send" type="submit">Ответить</button>
     </form>
   </section>`
 );
 
+const defaultModel = {
+  question: 'Выберите инди-рок треки',
+  answers: [
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4},
+  ]
+};
+
 const open = () => {
-  setScreen(getLevelGenreScreen());
+  let screen = getLevelGenreScreen(defaultModel);
+  setScreen(screen);
+
   bindAnswers();
   bindSubmit();
 };
